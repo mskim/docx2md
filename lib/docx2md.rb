a67542bcc80c985b8bf3bcc26711e1f9b5ab09a2
Min Soo Kim <mskimsid@gmail.com>
2023-01-01 17:09:54 +0900

    initial commit

---
+++ lib/docx2md.rb
@@ -0,0 +1,7 @@
+require 'docx2md/version'
+
+module Docx2md #:nodoc:
+  autoload :Document, 'docx2md/document'
+end
+
+require 'docx2md/core_ext/module'
---
+++ lib/docx2md/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Docx2md2md #:nodoc:
+  VERSION = '0.7.0'
+end
